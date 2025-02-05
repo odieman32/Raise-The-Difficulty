611e9efdf7210a695a8861881d35b5738f4513ae
odieman32 <dettermanbrodie@gmail.com>
2025-02-04 21:57:19 -0500

    New movement system

--- Raise The Difficulty/Assets/Scripts/CodeCredits.cs
+++ Raise The Difficulty/Assets/Scripts/CodeCredits.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CodeCredits : MonoBehaviour
 {
-    //Digital Oddities https://www.youtube.com/watch?v=l-Bbwvk3RXY&list=PLvdaHZw6zzmt94YFtnfzTLtgkyZlNh9go&index=5
     //Chris' Tutorials https://www.youtube.com/watch?v=7iYWpzL9GkM
 }
 
