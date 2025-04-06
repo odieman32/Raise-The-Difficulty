a92476c3fcbcbf236995f88fa1e2218c931e2f8e
odieman32 <dettermanbrodie@gmail.com>
2025-04-05 20:44:49 -0400

    UI Updates, New Boss Start

--- Raise The Difficulty/Assets/Scripts/MainMenu.cs
+++ Raise The Difficulty/Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainWindow;
+    public GameObject optionsWindow;
+    public GameObject controlsWindow;
+    [SerializeField] RectTransform mainWindowRect, optionsWindowRect, controlsWindowRect;
+    [SerializeField] float topPosY, middlePosY;
+    [SerializeField] float tweenDur;
+
     public void PlayGame()
     {
         Debug.Log("Start Game!");
@@ -12,6 +20,54 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
         Time.timeScale = 1f;
     }
+
+    //public void OptionsBack()
+    //{
+    //    MainIntro();
+    //    optionsWindow.SetActive(false);
+    //    mainWindow.SetActive(true);
+    //}
+    //public void Options()
+    //{
+    //    OptionsIntro();
+    //    optionsWindow.SetActive(true);
+    //    mainWindow.SetActive(false);
+    //}
+
+    //public void Controls()
+    //{
+    //    ControlsIntro();
+    //    controlsWindow.SetActive(true);
+    //    mainWindow.SetActive(false);
+    //}
+
+    //public void ControlsBack()
+    //{
+    //    MainIntro();
+    //    controlsWindow.SetActive(false);
+    //    mainWindow.SetActive(true);
+    //}
+
+    //void OptionsIntro()
+    //{
+    //    optionsWindowRect.DOAnchorPosY(middlePosY, tweenDur).SetUpdate(true);
+    //    mainWindowRect.DOAnchorPosY(topPosY, tweenDur).SetUpdate(true);
+    //}
+
+    //void MainIntro()
+    //{
+    //    mainWindowRect.DOAnchorPosY(middlePosY, tweenDur).SetUpdate(true);
+    //    optionsWindowRect.DOAnchorPosY(topPosY, tweenDur).SetUpdate(true);
+    //    controlsWindowRect.DOAnchorPosY(topPosY, tweenDur).SetUpdate(true);
+    //}
+
+    //void ControlsIntro()
+    //{
+    //    controlsWindowRect.DOAnchorPosY(middlePosY, tweenDur).SetUpdate(true);
+    //    mainWindowRect.DOAnchorPosY(topPosY, tweenDur).SetUpdate(true);
+    //}
+
+
 }
 
 //by Brodie Detterman
